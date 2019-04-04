e903e1f4b9658f3a6d5ecdd078f43991d8262e41
William Barnes <william.h.barnes@outlook.com>
2019-04-04 10:59:58 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -36,7 +36,12 @@ $(".theButton").mouseout(function(){
 	$(this).removeClass("makeBlack");
    });
 
-      
+//will display rgb value of selected panel in the reset panel
+$(".theButton").click(function(){
+    var col = $(this).css('background-color');
+ $('.superButton').text( col);
+
+   });      
 
 
 });
